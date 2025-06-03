ee4491519aa26fb8e261c9476b8689d88576a7a1
Kaue Antonio Maranzano de Araujo <kauearaujo@usp.br>
2025-06-02 23:44:25 -0300

    indef?

--- exerc6/heap.h
+++ exerc6/heap.h
@@ -1,9 +1,10 @@
-
-
+#ifndef HEAP_H
+#define HEAP_H 
 
 void heapify( int* heap_max, int tam, const int j); //função que verifica se o pai é maior que os filhos, se não, troca pelo maior filho
 
 
-void* heapmax ( int* vetor, int tam); //função que transforma o heap em heap máximo
+void heapmax ( int* vetor, int tam); //função que transforma o heap em heap máximo
  
 void heapsort (int* vetor, int tam ); //função que ordena o vetor usando heapsort
+#endif
