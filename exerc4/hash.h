049f10f10ff7aedf8d02ef8e83e8f60d9a3d471f
Mateus Claudino <mateus.alves.martins.claudino@hotmail.com>
2025-06-08 17:36:26 -0300

    commented exerc4 files and added grupo.txt with the names and nusp of every groupmember

--- exerc4/hash.h
+++ exerc4/hash.h
@@ -8,20 +8,97 @@ typedef struct hash
     FILE* fhash;
     char* fpath;
     unsigned int capacity; // m
-    list** student_list; 
+    list** student_list; // linked list of students
 } hash;
 
-hash* new_hash();
-void free_hash(hash* hash_table);
-void initialize_hash(hash* hash_table, unsigned int capacity, const char* path);
+/**
+ * creates a new hash pointer
+ * @return pointer to new allocated hash table
+*/
+hash* new_hash(); 
+
+/**
+ * frees the memory allocated to a hash table
+ * @param hash_table hash pointer to be freed
+*/
+void free_hash(hash* hash_table); 
+
+/**
+ * initalizes a hash table starting inside objects to NULL 
+ * and setting capacity to 'capacity'
+ * @param hash_table hash to initialize
+ * @param capacity hash_table maximum capacity
+ * @param path path to the .txt file that stores the hash_table elements
+*/
+void initialize_hash(hash* hash_table, unsigned int capacity, const char* path); 
+
+/**
+ * calculates the position and inserts a new student to the hash table
+ * @param hash_table hash to insert
+ * @param new_student student that will be added on the hash using its name as hash
+*/
 void hash_insert(hash* hash_table, student new_student);
+
+/**
+ * removes a student from the list that matches the name passed as argument
+ * @param hash_table hash to remove
+ * @param name name of the student that is going to be removed (if exists)
+*/
 void hash_remove(hash* hash_table, const char* name);
+
+/**
+ * searches for a specific student with name 'name' inside the hash 
+ * and print its informations if found
+ * @param hash_table hash to search
+ * @param name name of the target student
+*/
 void hash_search(hash* hash_table, const char* name);
+
+/**
+ * saves all hash_table content to .txt file with path 'path'
+ * @param hash_table hash to save to the file
+ * @param path path of the .txt file to dave the hash
+*/
 void save_hash_file(hash* hash_table, const char* path);
+
+/**
+ * loads the contents of a .txt file with path 'path' and puts it into
+ * the hash table passed as argument
+ * @param hash_table - hash where contents will be copied to
+ * @param path - path of the .txt file to be read
+*/
 int load_hash_file(hash* hash_table, const char* path);
+
+/**
+ * hash function that returns the key to a student with name 'name'.
+ * It uses the name of the student as the hash
+ * @param name name to hash
+ * @return key of the given name on the hash
+*/
 unsigned int h1(const char* name);
+
+/**
+ * takes the output of the h1() function and then returns a position
+ * on the hash to put the given student name using modulo
+ * @param capacity hash capacity to use as modulo
+ * @param name name of the student to hash
+ * @return position on the vector of a hash_table 
+*/
 unsigned int h2(unsigned int capacity, const char* name);
+
+/**
+ * rotates the binary representation of a given 'value' by 'n_bits' counterclockwise
+ * @param value the number to be rotated
+ * @param n_bits number of bits to rotate 'value'
+ * @return returns the corresponding unsigned int value of the rotation of 'n_bits' bits of 'value'  
+*/
 unsigned int rotl(unsigned int value, unsigned int n_bits);
+
+/**
+ * counts the number of lines a .txt has
+ * @param path path to the file
+ * @return number of lines of file inside 'path'
+*/
 unsigned int count_file_lines(const char* path);
 
 #endif
--- exerc4/list.h
+++ exerc4/list.h
@@ -18,11 +18,48 @@ typedef struct list {
     int list_size;
 } list;
 
+/**
+ * creates a new singly linked list
+ * @return pointer to the new list created
+*/
 list* create_list();
-void free_list(list*);
-void insert_element(list*, student);
-void remove_element(list*, const char*);
-student* search(list*, const char*);
-student copy_student(const student*); 
+
+/**
+ * frees a previously allocated list
+ * @param lst pointer to the list that will be freed 
+*/
+void free_list(list* lst);
+
+/**
+ * inserts a student at the beginning of a list
+ * @param lst the list to add the student
+ * @param new_student the new student to add
+*/
+void insert_element(list* lst, student new_student);
+
+/**
+ * removes the first student with the specified name from the list
+ * @param lst the list that will be searched
+ * @param name_to_remove the name of the student to be removed
+*/
+void remove_element(list* lst, const char* name_to_remove);
+
+/**
+ * searches through a list for a specific student name 
+ * and the returns a pointer to the found student or NULL 
+ * if not found
+ * @param lst the list to search
+ * @param target_name name of the target student
+ * @return pointer to the found student or NULL if not
+*/
+student* search(list* lst, const char* target_name);
+
+/**
+ * copies the contents of a student to another one and 
+ * returns it (mainly used to add new students to lists)
+ * @param source student to be copied
+ * @return new student with the copied informations
+*/
+student copy_student(const student* source); 
 
 #endif
