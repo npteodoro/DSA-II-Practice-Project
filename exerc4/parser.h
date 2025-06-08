8b801980449b7a0c2cd808ca807aac41ed5aac2f
Mateus Claudino <164416681+mateus-claudino@users.noreply.github.com>
2025-06-08 18:00:04 -0300

    commented exerc4 files and added grupo.txt with the names and nusp of every groupmember (#10)

--- exerc4/parser.h
+++ exerc4/parser.h
@@ -24,8 +24,27 @@ typedef struct parser
     };
 } parser;
 
+/**
+ * creates a new parser object to parse some text from the command line
+ * @param arg_count number of arguments to parse
+ * @return pointer to a new parser
+*/
 parser* new_parser(int arg_count);
+
+/**
+ * frees the allocated memory to a parser
+ * @param p pointer to the parser that needs to be freed
+*/
 void free_parser(parser* p);
+
+/**
+ * parses the command line arguments passed to the code and 
+ * returns it to a parser, so that the important informations can
+ * be more easily used 
+ * @param arg_count number of command line arguments
+ * @param arg_values array of the command line arguments
+ * @return a parser with all the information separated 
+*/
 parser* parse_command_line_arguments(int arg_count, char* arg_values[]);
 
 #endif
